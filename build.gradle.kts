6d917294962dc66aeadee1bae716a2a2d2921e8a
Roman Schaller <roman.schaller@gmail.com>
2020-10-02 23:11:09 +0200

    Enable Windows builds containing jre

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import de.undercouch.gradle.tasks.download.Download
 
 plugins {
     java
     id("application")
     id("io.github.fvarrui.javapackager.plugin") version("1.2.0")
+    id ("de.undercouch.download") version("4.1.1")
 }
 
 java {
@@ -56,14 +58,36 @@ tasks.create<io.github.fvarrui.javapackager.gradle.PackageTask>("packageLinux") 
     linuxConfig
 }
 
+tasks {
+    val downloadJre by registering(Download::class) {
+        src("https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.8%2B10/OpenJDK11U-jre_x64_windows_hotspot_11.0.8_10.zip")
+        dest(File(buildDir, "win64-jre.zip"))
+        onlyIfModified(true)
+    }
+}
+
+tasks.register<Copy>("unpackJre") {
+    dependsOn("downloadJre")
+    from(zipTree("$buildDir/win64-jre.zip")) {
+        include("jdk*/**")
+        eachFile {
+            relativePath = RelativePath(true, *relativePath.segments.drop(1).toTypedArray())
+        }
+        includeEmptyDirs = false
+    }
+    into(File(buildDir, "win64-jre"))
+}
+
 tasks.create<io.github.fvarrui.javapackager.gradle.PackageTask>("packageWindows") {
     group = "distribution"
-    dependsOn("jar")
+    dependsOn("unpackJre", "jar")
     mainClass = "ch.romix.schirizettel.generator.GeneratorGUI"
     assetsDir = file("./build/libs")
     isBundleJre = true
+    jrePath = File(buildDir, "win64-jre")
     platform = io.github.fvarrui.javapackager.model.Platform.windows
     isGenerateInstaller = false
     isCreateZipball = true
     winConfig
 }
+
